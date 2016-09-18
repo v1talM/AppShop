43d3cd8b5be4c191005b1bfa3d3da0a7b7f53ce1
v1talM <373357042@qq.com>
2016-09-18 18:37:20 +0800

    user migration update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->comment('昵称');
+            $table->string('email')->unique()->comment('登录邮箱');
             $table->string('password');
+            $table->string('avatar')->comment('用户头像');
+            $table->integer('score')->default('0')->comment('积分');
+            $table->integer('total_score')->default('0')->comment('积分总计');
+            $table->enum('level',['1','2','3','4','5'])->default('1')->comment('会员等级');
+            $table->enum('is_banned',['1','0'])->default('0')->comment('是否可用');
             $table->rememberToken();
             $table->timestamps();
         });
