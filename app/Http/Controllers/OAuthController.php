5e146fbd94abbe71bb7415ca62b425d9c83e9410
v1talM <373357042@qq.com>
2016-10-25 18:43:43 +0800

    登录判断修改返回信息提示

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -25,27 +25,33 @@ class OAuthController extends Controller
 
     public function oauth(Request $request)
     {
-        $response = $this->http->post('http://119.29.5.221/oauth/token', [
-            'form_params' => [
-                'grant_type' => 'password',
-                'client_id' => 9,
-                //本地
-                //'client_secret' => 'uNnM7S2dk7Et1Fhbld8t0pkTJNucH85qxUlQK2s3',
-                //服务器上
-                'client_secret' => 'W1GsIEpXqRbYn2ZrcT8hsQt0rCYFhR8lwPagR5Uf',
-                'username' => $request->input('username'),
-                'password' => $request->input('password'),
-                'scope' => '',
-            ],
-        ]);
+
+        try {
+            $response = $this->http->post('http://119.29.5.221/oauth/token', [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => 9,
+                    //本地
+                    //'client_secret' => 'uNnM7S2dk7Et1Fhbld8t0pkTJNucH85qxUlQK2s3',
+                    //服务器上
+                    'client_secret' => 'W1GsIEpXqRbYn2ZrcT8hsQt0rCYFhR8lwPagR5Uf',
+                    'username' => $request->input('username'),
+                    'password' => $request->input('password'),
+                    'scope' => '',
+                ],
+            ]);
+        }catch (\Exception $e){
+            return response()->json(['status' => 401, 'message' => '用户名和密码不匹配']);
+        }
         //return json_decode((string) $response->getBody(), true);
         $accessToken =  Arr::get(json_decode((string) $response->getBody(), true),'access_token');
-        return response()->json(['status' => 200 , 'accessToken' => $accessToken]);
+        return response()->json(['status' => 200 , 'accessToken' => $accessToken , 'message' => '登录成功']);
 
     }
 
     public function getUserByToken(Request $request)
     {
-        return json_decode((string) $request->user(), true);
+        $user = $request->user();
+        return response()->json(['status' => 200, 'user' => $user, 'message' => '获取用户信息成功']);
     }
 }
